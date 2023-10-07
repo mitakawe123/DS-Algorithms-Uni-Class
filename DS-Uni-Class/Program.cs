82871b11cdb7085bfb297da8bfe91c1eee28b7f7
Dimitar Kyuchukov <dkyuchukov2003@gmail.com>
2023-10-07 12:25:33 +0300

    Add project files.

---
+++ DS-Uni-Class/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DS_Uni_Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
