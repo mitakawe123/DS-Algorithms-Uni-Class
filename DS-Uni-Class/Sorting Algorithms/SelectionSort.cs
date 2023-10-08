c32c4f8ddde3f0c6e6e7ae941791a1db74bc8ebd
Dimitar Kyuchukov <dkyuchukov2003@gmail.com>
2023-10-08 10:37:25 +0300

    Selection sort

---
+++ DS-Uni-Class/Sorting Algorithms/SelectionSort.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS_Uni_Class.Sorting_Algorithms
+{
+    public static class SelectionSort
+    {
+        // най лошия случай е O(N^2)
+        // работи на принципа че ще обиколи целия масив ще намери най малкото число и ще го постави на неговото място
+        public static void DKSelectionSort(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j < arr.Length - 1; j++)
+                    if (arr[j] < arr[min])
+                        min = j;
+
+                int temp = arr[i];
+                arr[i] = arr[min];
+                arr[min] = temp;
+            }
+        }
+    }
+}
