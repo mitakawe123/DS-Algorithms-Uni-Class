d7e06eb0068acfe6b12f4394812435b878e42ebc
Dimitar Kyuchukov <dkyuchukov2003@gmail.com>
2023-10-08 10:39:17 +0300

    fix

--- DS-Uni-Class/Sorting Algorithms/SelectionSort.cs
+++ DS-Uni-Class/Sorting Algorithms/SelectionSort.cs
@@ -12,10 +12,10 @@ namespace DS_Uni_Class.Sorting_Algorithms
         // работи на принципа че ще обиколи целия масив ще намери най малкото число и ще го постави на неговото място
         public static void DKSelectionSort(int[] arr)
         {
-            for (int i = 0; i < arr.Length; i++)
+            for (int i = 0; i < arr.Length - 1; i++)
             {
                 int min = i;
-                for (int j = i + 1; j < arr.Length - 1; j++)
+                for (int j = i + 1; j < arr.Length; j++)
                     if (arr[j] < arr[min])
                         min = j;
 
